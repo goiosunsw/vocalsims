6547f680162659b87f64e3f41d83dd9437ecfd40
Andre Goios <a.almeida@unsw.edu.au>
2019-12-02 18:22:52 +1100

    transfer functions with subglottal tract

---
+++ subglottal_tf.py
@@ -0,0 +1,6 @@
+import pympedance.Synthesiser as psyn
+from scipy.optimize import minimize
+
+__simulation_type__ = "freq-domain simulation with subglottal tract (Hanson & Stevens 1996)"
+__simulation_version__ = "20191128"
+__parameter_version__ = "20191128"
