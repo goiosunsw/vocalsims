2f0ad3d6e14eb253d3e2ec7bf61cc28ebabeefd5
Andre Goios <a.almeida@unsw.edu.au>
2019-12-03 19:19:03 +1100

    json object functionality

--- json_object.py
+++ json_object.py
@@ -3,12 +3,13 @@ import json
 class JSONObject(object):
     def __init__(self, json=None):
         self.max_repr_chars=160
+        self._json={}
         if json is not None:
             self._json = json
-    def __getattr__(self, key):
-        key = key.split('/')
-        return self.__getitem__(key)
+
     def _normalise_key(self,key):
+        if key[0] == '/':
+            key = key[1:]
         if isinstance(key,int):
             key = str(key)
         if isinstance(key,str):
@@ -29,14 +30,16 @@ class JSONObject(object):
                 if isinstance(el,str):
                     return el
                 else:
-                    return JSONObject(json=el)
+                    return self.__class__(json=el)
             else:
                 return el
         except TypeError:
             return el
+
     def __setitem__(self,key,value):
         key = self._normalise_key(key)
         el = self._json
+        k2 = key[-1]
         for k1, k2 in zip(key[:-1],key[1:]):
             try:
                 k1 = int(k1)
@@ -63,34 +66,45 @@ class JSONObject(object):
         try:       
             el[k2] = value
         except IndexError:
-            for ii in range(len(el),path[-1]):
+            for ii in range(len(el),key[-1]):
                 el.append({})
+
     def __len__(self):
         return len(self._json)
+
     def items(self):
         try:
-            yield from self._json.items()
+            for k, v in self._json.items():
+                yield k, self.__class__(v)
         except AttributeError:
             for kk, vv in enumerate(self._json):
                 yield kk, vv
+
     def read_file(self,filename):
         with open(filename,'r') as f:
             self._json = json.load(f)    
+
     def iter_tree(self):
         self._cur_path = []
         yield from self._iter_tree()
+
     def _iter_tree(self):
         cur_node = self[self._cur_path]
         cur_path = self._cur_path
+        print("-- "+str(cur_path))
         try:
             len(cur_node) == 0
         except TypeError:
-            yield self._cur_path, cur_node
-        
-        if isinstance(cur_node, JSONObject):
-            for key, value in cur_node.items():
-                self._cur_path = cur_path + [key]
-                yield from self._iter_tree()
+            yield cur_path, cur_node
+        else:    
+            if isinstance(cur_node, self.__class__):
+                for key, value in cur_node.items():
+                    self._cur_path = cur_path + [key]
+                    yield from self._iter_tree()
+            else:
+                yield cur_path, cur_node
+            
+                #self._cur_path = cur_path
 #         if isinstance(cur_node, dict):
 #             if len(cur_node) == 0:
 #                 yield cur_path, cur_node
@@ -101,19 +115,48 @@ class JSONObject(object):
 #             for key, value in enumerate(cur_node):
 #                 self._cur_path = cur_path + [key]
 #                 yield from self._iter_tree()
+
     def visit(self,fun):
-        ret = []
         for k, v in self.iter_tree():
-            ret.append(fun(v))
-        return ret
+            fun(v)
+
+    def visititems(self, fun):
+        for k, v in self.iter_tree():
+            fun(k,v)
+    
     def __repr__(self):
-        ret = 'JSONObject:\n'
+        ret = str(self.__class__)+':\n'
         jsstr = str(self._json)
         if len(jsstr)>self.max_repr_chars:
             nch = self.max_repr_chars//2 - 2
             jsstr = jsstr[:nch]+' ... '+jsstr[-nch:]
         ret += jsstr
         return ret 
+            
+    def copy(self):
+        newj = self.__class__()
+        for k,v in self.iter_tree():
+            newj[k] = v
+        return newj
+
+    def _get_raw_json_parent(self,key):
+        key = self._normalise_key(key)
+        el = self._json
+        for kk in key[:-1]:
+            el = el[kk] 
+        return el
+
+    def __delitem__(self,key):
+        key = self._normalise_key(key)
+        parent = self._get_raw_json_parent(key)
+        del parent[key[-1]]
+
+    def pop(self, key):
+        key = self._normalise_key(key)
+        parent = self._get_raw_json_parent(key)
+        ret = parent[key[-1]]
+        del parent[key[-1]]
+        return self.__class__(ret)
             
             
         
