13fd4bca81a0b72db8fb2664d2aaf7d491106234
Andre Almeida <a.almeida@unsw.edu.au>
2020-07-08 15:10:23 +1000

    cythonized delay lines

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ import distutils.core
 import Cython.Build
 import numpy
 distutils.core.setup(
-    ext_modules = Cython.Build.cythonize("real_time_filt.pyx",annotate=True),
+    ext_modules = Cython.Build.cythonize(["*.pyx"]),
+                  
     include_dirs=[numpy.get_include()])
