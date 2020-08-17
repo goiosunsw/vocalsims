904fbe66bec77849520d3668fe29888ec65358a0
Andre Almeida <a.almeida@unsw.edu.au>
2020-08-17 15:35:31 +1000

    first stage of reorganisation

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 import distutils.core
 import Cython.Build
 import numpy
+
+
+import unittest
+def my_tests():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
 distutils.core.setup(
-    ext_modules = Cython.Build.cythonize(["*.pyx"]),
+    name = 'vocalsims',
+    version = '0.0.2',
+    description = 'vocal and lip instrment simulations',
+    author = 'Andre Goios',
+    license='GPL v3',
+    packages=['vocalsims', 'vocalsims.interfacing','vocalsims.fitting'],
+    test_suite = 'setup.my_tests',
+    ext_modules = Cython.Build.cythonize(["vocalsims/*.pyx"]),
                   
     include_dirs=[numpy.get_include()])
